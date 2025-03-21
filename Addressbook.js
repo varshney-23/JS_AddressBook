aa948cf9cc2dea3d6fda0e4db975fe3c18495a32
varshney-23 <ayushivarshney40@gmail.com>
2025-03-21 12:05:26 +0530

    alphabetically sorted

--- Addressbook.js
+++ Addressbook.js
@@ -5,61 +5,41 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// Object to hold multiple Address Books
-const addressBooks = {};
+const addressBook = [];
 
-//  Function to check for duplicate contact
-function isDuplicateContact(bookName, firstName, lastName) {
-    return addressBooks[bookName].some(
-        (contact) => contact.firstName === firstName && contact.lastName === lastName
-    );
+//  Function to format contact as a string
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    //  Override toString() method
+    toString() {
+        return `${this.firstName} ${this.lastName}, ${this.address}, ${this.city}, ${this.state}, ${this.zip}, ${this.phone}, ${this.email}`;
+    }
 }
 
-// Function to add a contact to an Address Book
-function addContactToAddressBook() {
-    rl.question("Enter the Address Book name: ", (bookName) => {
-        if (!addressBooks[bookName]) {
-            console.log(` Address Book '${bookName}' does not exist.`);
-            showMenu();
-            return;
-        }
-
-        rl.question("Enter First Name: ", (firstName) => {
-            rl.question("Enter Last Name: ", (lastName) => {
-
-                //  Check for duplicates using `some()`
-                if (isDuplicateContact(bookName, firstName, lastName)) {
-                    console.log(` Duplicate contact '${firstName} ${lastName}' already exists in '${bookName}'.`);
-                    showMenu();
-                    return;
-                }
-
-                rl.question("Enter Address: ", (address) => {
-                    rl.question("Enter City: ", (city) => {
-                        rl.question("Enter State: ", (state) => {
-                            rl.question("Enter Zip: ", (zip) => {
-                                rl.question("Enter Phone Number: ", (phone) => {
-                                    rl.question("Enter Email: ", (email) => {
-                                        try {
-                                            const contact = {
-                                                firstName,
-                                                lastName,
-                                                address,
-                                                city,
-                                                state,
-                                                zip,
-                                                phone,
-                                                email
-                                            };
-
-                                            //  Add contact if no duplicate is found
-                                            addressBooks[bookName].push(contact);
-                                            console.log(` Contact added to '${bookName}' successfully!`);
-                                        } catch (error) {
-                                            console.error(`\n Error: ${error.message}`);
-                                        }
-                                        showMenu();
-                                    });
+//  Function to add a new contact
+function addContact() {
+    rl.question("Enter First Name: ", (firstName) => {
+        rl.question("Enter Last Name: ", (lastName) => {
+            rl.question("Enter Address: ", (address) => {
+                rl.question("Enter City: ", (city) => {
+                    rl.question("Enter State: ", (state) => {
+                        rl.question("Enter Zip: ", (zip) => {
+                            rl.question("Enter Phone Number: ", (phone) => {
+                                rl.question("Enter Email: ", (email) => {
+                                    const contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+                                    addressBook.push(contact);
+                                    console.log(` Contact '${firstName} ${lastName}' added successfully.`);
+                                    showMenu();
                                 });
                             });
                         });
@@ -70,72 +50,65 @@ function addContactToAddressBook() {
     });
 }
 
-//  Function to count contacts by city and state using `reduce`
-function countContactsByLocation() {
-    rl.question("Enter the Address Book name: ", (bookName) => {
-        if (!addressBooks[bookName]) {
-            console.log(` Address Book '${bookName}' does not exist.`);
-            showMenu();
-            return;
-        }
-
-        //  Count by city
-        const cityCount = addressBooks[bookName].reduce((acc, contact) => {
-            acc[contact.city] = (acc[contact.city] || 0) + 1;
-            return acc;
-        }, {});
-
-        //  Count by state
-        const stateCount = addressBooks[bookName].reduce((acc, contact) => {
-            acc[contact.state] = (acc[contact.state] || 0) + 1;
-            return acc;
-        }, {});
-
-        // Display count by city
-        console.log("\n Number of contacts by City:");
-        Object.keys(cityCount).forEach(city => {
-            console.log(`${city}: ${cityCount[city]}`);
-        });
-
-        //  Display count by state
-        console.log("\n Number of contacts by State:");
-        Object.keys(stateCount).forEach(state => {
-            console.log(` ${state}: ${stateCount[state]}`);
-        });
-
+//  Function to display all contacts
+function displayContacts() {
+    if (addressBook.length === 0) {
+        console.log(" No contacts available.");
         showMenu();
+        return;
+    }
+    console.log("\n=== Address Book ===");
+    addressBook.forEach((contact, index) => {
+        console.log(`${index + 1}. ${contact.toString()}`);
     });
 }
 
-// Function to display the menu
+// Function to sort contacts alphabetically by Person’s name
+function sortByName() {
+    if (addressBook.length === 0) {
+        console.log(" No contacts available to sort.");
+        showMenu();
+        return;
+    }
+
+    console.log("\n Contacts sorted alphabetically by name:");
+    
+    // Use `sort()` and `localeCompare()` for alphabetical sorting
+    addressBook
+        .slice() // Create a copy to avoid modifying the original
+        .sort((a, b) => (a.firstName + " " + a.lastName).localeCompare(b.firstName + " " + b.lastName))
+        .forEach(contact => console.log(contact.toString()));
+
+    showMenu();
+}
+
 function showMenu() {
     console.log("\n=== Address Book Menu ===");
-    console.log("1. Create New Address Book");
-    console.log("2. Add Contact to Address Book");
-    console.log("3. Count Contacts by City/State"); 
+    console.log("1. Add Contact");
+    console.log("2. Display Contacts");
+    console.log("3. Sort Contacts by Name");
     console.log("4. Exit");
 
     rl.question("Enter your choice: ", (choice) => {
         switch (choice) {
             case '1':
-                createAddressBook();
+                addContact();
                 break;
             case '2':
-                addContactToAddressBook();
+                displayContacts();
                 break;
             case '3':
-                countContactsByLocation(); 
+                sortByName();
                 break;
             case '4':
-                console.log("Exiting Address Book. Goodbye!");
+                console.log(" Exiting Address Book. Goodbye!");
                 rl.close();
                 break;
             default:
-                console.log("Invalid choice. Please try again.");
+                console.log(" Invalid choice. Please try again.");
                 showMenu();
         }
     });
 }
 
-// Start the program
 showMenu();
